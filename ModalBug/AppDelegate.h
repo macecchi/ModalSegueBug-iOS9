67a2301332d810ad7377709b074f7f0af762a673
Mario Cecchi <macecchi@gmail.com>
2015-09-23 23:17:24 -0300

    Initial Commit

---
+++ ModalBug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModalBug
+//
+//  Created by Mario Cecchi on 23/09/2015.
+//  Copyright © 2015 Mario Cecchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModalBug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModalBug
+//
+//  Created by Mario Cecchi on 23/09/2015.
+//  Copyright © 2015 Mario Cecchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
